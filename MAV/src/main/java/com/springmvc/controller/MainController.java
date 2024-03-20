0d320ff0dbe00ac4d18957863e9ef0de04770280
PMS <bminsu951106@gmail.com>
2024-03-20 10:12:02 +0900

    2024-03-20

---
+++ MAV/src/main/java/com/springmvc/controller/MainController.java
@@ -0,0 +1,47 @@
+package com.springmvc.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.springmvc.domain.MatchRoom;
+import com.springmvc.domain.Room;
+import com.springmvc.service.ClubService;
+import com.springmvc.service.MatchService;
+import com.springmvc.service.StoreService;
+
+@Controller
+public class MainController {
+   
+    @Autowired
+    private StoreService storeService;
+    
+   @Autowired
+   private MatchService matchService;
+   
+   @Autowired
+   private ClubService clubService;
+
+   // 메인 페이지 호출
+   @GetMapping("/main")
+   public String goHome(Model model,Room room,MatchRoom matchRoom) {   
+      
+      //매칭룸
+       List<MatchRoom> matchView = matchService.findAllMatchRooms(matchRoom);
+       model.addAttribute("matchView",matchView);
+       
+       
+       //경기장
+        List<Room> myRooms = storeService.getAllRooms(room);
+        model.addAttribute("myRooms", myRooms);
+       
+      return "main";
+   }
+
+}
+
